89f36a7f01fcfe25ab8379c8bf3b1940fd837185
SerKo <serko.dev@gmail.com>
2023-12-15 18:26:35 +0800

    feat: add vue monorepo

---
+++ vue/bar/src/components/TheWelcome.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+// Error when import by foo due to path alias usage
+import { message } from '@/utils/message'
+</script>
+
+<template>
+  <div>{{ message }}</div>
+</template>
---
+++ vue/foo/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import BarWelcome from '@ts-monorepo/bar/src/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <BarWelcome />
+  </main>
+</template>
